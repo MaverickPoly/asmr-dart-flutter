188c5eb72fe99844a4c8ebb026b643e33f9bbc8d
Maverick <realahmadjon82@gmail.com>
2025-08-05 22:47:14 +0500

    app_08_news

--- app_08_news/lib/services/news_service.dart
+++ app_08_news/lib/services/news_service.dart
@@ -4,7 +4,7 @@ import 'package:app_08_news/models/new.dart';
 import "package:http/http.dart" as http;
 
 class NewsService {
-  String apiKey = "1b1ae630e92248b2bd89e3661a0bf843";
+  String apiKey = "";
 
   Future<List<New>> fetchNews() async {
     var response = await http.get(
@@ -21,7 +21,6 @@ class NewsService {
     List<New> articles = [];
 
     for (var element in articlesJson) {
-      print(element);
       New newItem = New(
         title: element["title"],
         description: element["description"],
